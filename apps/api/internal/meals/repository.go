e392c70b182c907e6b144f23f7774cfc63fd521e
jonahan93 <149839345+sangkihan93@users.noreply.github.com>
2026-06-14 11:17:53 -0400

    Add meal repository interface

--- apps/api/internal/meals/repository.go
+++ apps/api/internal/meals/repository.go
@@ -1,1 +1,8 @@
 package meals
+
+import "context"
+
+type Repository interface {
+	Create(ctx context.Context, MealLog MealLog) (MealLog, error)
+	FindByUserID(ctx context.Context, userID string) ([]MealLog, error)
+}
